5f6dbf2d6e8629587204e68971fc3a0637fc70dd
Daniel Vasquez <danielgusvt@yahoo.com>
2019-05-14 20:29:22 +0000

    sdm660-common: Adapt HALs inclusion
    Rewrite makefile to correspond payton.
    
    Signed-off-by: Daniel Vasquez <danielgusvt@yahoo.com>

--- hardware/Android.mk
+++ hardware/Android.mk
@@ -1,43 +1,25 @@
-ifneq ($(wildcard device/sony/common/hardware/qcom/custom.mk),)
-    include device/sony/common/hardware/qcom/custom.mk
-else
 # Board platforms lists to be used for
 # TARGET_BOARD_PLATFORM specific featurization
-QCOM_BOARD_PLATFORMS += msm8952 msm8996 msm8998 sdm660 sdm845
-
-QCOM_NEW_MEDIA_PLATFORM := sdm845
+QCOM_BOARD_PLATFORMS += msm8998 sdm660
 
 #List of targets that use video hw
-MSM_VIDC_TARGET_LIST := msm8952 msm8996 msm8998 sdm660 sdm845
+MSM_VIDC_TARGET_LIST := msm8998 sdm660
 
 #List of targets that use master side content protection
-MASTER_SIDE_CP_TARGET_LIST := msm8996 msm8998 sdm660 sdm845
+MASTER_SIDE_CP_TARGET_LIST := msm8998 sdm660
 
-audio-hal := hardware/qcom/audio
-gps-hal := hardware/qcom/gps/sdm845
-ipa-hal := hardware/qcom/data/ipacfg-mgr/sdm845
-
-display-hal := hardware/qcom/display/sde
-
-ifneq ($(filter $(QCOM_NEW_MEDIA_PLATFORM), $(TARGET_BOARD_PLATFORM)),)
-QCOM_MEDIA_ROOT := hardware/qcom/media/sdm845
-else
 QCOM_MEDIA_ROOT := hardware/qcom/media/msm8998
-endif
 
 OMX_VIDEO_PATH := mm-video-v4l2
 media-hal := $(QCOM_MEDIA_ROOT)
 
-SRC_CAMERA_HAL_DIR ?= vendor/qcom/opensource/camera
-SRC_DISPLAY_HAL_DIR := $(display-hal)
-SRC_MEDIA_HAL_DIR := $(QCOM_MEDIA_ROOT)
-TARGET_KERNEL_VERSION := $(SOMC_KERNEL_VERSION)
+# This defs are used on the camera hal, may not be needed
+#SRC_CAMERA_HAL_DIR ?= vendor/qcom/opensource/camera
+# SRC_DISPLAY_HAL_DIR := hardware/qcom/display/msm8998
+# SRC_MEDIA_HAL_DIR := $(QCOM_MEDIA_ROOT)
 
 include device/sony/common/hardware/qcom/utils.mk
 
-include $(display-hal)/Android.mk
-include $(call all-makefiles-under,$(audio-hal))
-include $(call first-makefiles-under,$(ipa-hal))
-include $(call all-makefiles-under,$(gps-hal))
+include hardware/qcom/display/aosp/msm8998/Android.mk
 include $(call all-makefiles-under,$(media-hal))
-endif
+#include hardware/qcom/media/msm8998/Android.mk
